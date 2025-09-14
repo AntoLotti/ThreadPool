a18eae616b96997ba4a07ac515d62f9cc9bca779
AntoLotti <antoniolottivillar@gmail.com>
2025-09-14 14:13:58 +0200

    Feat: Redefined threadpool parameters

--- includes/pthreadpool.h
+++ includes/pthreadpool.h
@@ -19,22 +19,27 @@
 //========TYPES===========//
 typedef struct task_s
 {
-    void *(*task_action)( void* arg );      //Pointer to a function that return a void pointer and recive a void pointer as argument
-    void *arg;                              //Pointer to the arguments of the functions
+    void  (*fn)(void* arg);     //Pointer to a function that recive a void pointer as argument
+    void* arg;                  //Pointer to the arguments of the functions
 }task_t;
 
 typedef struct pthreadpool_s
 {
-#ifdef DEBUG
-    int         thread_id;                  //Id of the thread
-#endif
+    pthread_t       threads[MAX_THREADS];   // An array of initialized threads 
+    pthread_mutex_t lock;                   // A mutex to synchronize access to the task queue. Ensures that only one thread at a time modifies the queue.
+    pthread_cond_t  notify;                 // A condition variable used to notify worker threads when new tasks are available.
+    task_t  task_queue[MAX_THREADS];        // A circular queue that store the new tasks
+    int     queue;                          // The number of tasks currently in the queue.
+    int     queue_front;                    // A index of the task queue, that point to the first elemment to use of the queue
+    int     queue_back;                     // A index of the task queue, that point to the next availabe space
+    int     stop;                           // A flag to indicate if the pool should stop executing (used for shutdown; true to stop ).
 }pthreadpool_t;
 
 
 //====FUN DECLARATIONS===//
 /**
  * @brief pthreadpool_init
- * @details function to initialice a threadpool
+ * @details function to initialice a pthreadpool
  * @author Antonio Lotti 
  * @date 04/09/2025
  * @param src pointer to a pthreadpool_t structure
@@ -43,8 +48,18 @@ typedef struct pthreadpool_s
 void pthreadpool_init( pthreadpool_t *src );
 
 /**
+ * @brief pthreadpool_destroy
+ * @details function to destroy a pthreadpool structure
+ * @author Antonio Lotti 
+ * @date 04/09/2025
+ * @param src pointer to a pthreadpool_t structure
+ * @return nothing
+ */
+void pthreadpool_destroy( pthreadpool_t *src );
+
+/**
  * @brief pthreadpool_add_task
- * @details function to add a task to the queue of the threadpool
+ * @details function to add a task to the queue of the pthreadpool
  * @author Antonio Lotti 
  * @date 04/039/2025
  * @param dst pointer to a pthreadpool_t structure
@@ -55,23 +70,12 @@ void pthreadpool_init( pthreadpool_t *src );
 void pthreadpool_add_task( pthreadpool_t *dst, void *(*fun)( void *arg ), void *arg );
 
 /**
- * @brief pthreadpool_destroy
- * @details function to destroy a threadpool structure
- * @author Antonio Lotti 
- * @date 04/09/2025
- * @param src pointer to a pthreadpool_t structure
- * @return nothing
- */
-void pthreadpool_destroy( pthreadpool_t *src );
-
-/**
- * @brief pthreadpool_assigner
- * @details function to assigne task to the threads 
+ * @brief pthreadpool_function
+ * @details This is function that will be runing in each thread
  * @author Antonio Lotti
- * @date 04/09/2025
- * @param src pointer to the pthreadpool_t
- * @return nothing
+ * @date 13/04/2025
+ * @param src A void pointer to a pthreadpool type
  */
-void* pthreadpool_assigner( void *src );
+void* pthreadpool_function(void* src);
 
 #endif
--- src/pthreadpool.c
+++ src/pthreadpool.c
@@ -2,18 +2,53 @@
 #include <pthreadpool.h>
 
 //========FUN DEC=========//
-static bool creator_pthreads( pthreadpool_t *src );
+static bool creator_pthreads( pthreadpool_t* src );
 static bool destructor_pthreads( pthreadpool_t *src );
 
 //========FUN DEF=========//
-void pthreadpool_init( pthreadpool_t* src )
+static bool creator_pthreads( pthreadpool_t* src )
 {
-    
+    for (size_t i = 0; i < MAX_THREADS; i++)
+    {
+        if (pthread_create(src->threads[i], NULL, pthreadpool_function, src ) != 0 )
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
-void pthreadpool_add_task( pthreadpool_t *dst, void *(*fun)( void *arg ), void *arg )
-{
+//static bool destructor_pthreads( pthreadpool_t *src );
 
+
+void pthreadpool_init( pthreadpool_t* src )
+{
+    if( src == NULL )
+    {
+        //TODO: Add error handler
+        return;
+    }
+
+    src->queue          = 0;
+    src->queue_front    = -1;
+    src->queue_back     = -1;
+    src->stop           = false;
+
+    pthread_mutex_init( &(src->lock), NULL);  
+    pthread_cond_init( &(src->notify), NULL);
+    
+    for (size_t i = 0; i < MAX_TASKS; i++)
+    {
+        src->task_queue[i].fn   = NULL;
+        src->task_queue[i].arg  = NULL;
+    }
+    
+    if (!creator_pthreads(src))
+    {
+        //TODO: create a error handler
+        return;
+    }
+    
 }
 
 void pthreadpool_destroy( pthreadpool_t *src )
@@ -21,7 +56,13 @@ void pthreadpool_destroy( pthreadpool_t *src )
 
 }
 
-void* pthreadpool_assigner( void *src )
+
+void pthreadpool_add_task( pthreadpool_t* dst, void* (*fun)(void*), void* arg)
+{
+
+}
+
+void* pthreadpool_function(void* src)
 {
 
 }
